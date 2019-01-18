c36d9a67ad583132e43bc544c35781f1aedef632
Yong Nam Kang <j.yongnamkang@gmail.com>
2019-01-18 00:59:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -157,7 +157,7 @@ end
 def player_stats(player_name)
   game_hash.each do |location, team_data|
     team_data[:players].each do |name, info|
-      return name if name == player_name
+      return info if name == player_name
     end
   end
 end
